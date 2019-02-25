da455c43bc6250eeeb29d87c0d60c19d9bb2d86d
wealchen <630121101@qq.com>
2019-02-25 14:51:40 +0800

    修复免密登录的一些显示bug

--- common/src/main/java/com/shanchain/data/common/net/SCHttpStringCallBack.java
+++ common/src/main/java/com/shanchain/data/common/net/SCHttpStringCallBack.java
@@ -14,6 +14,7 @@ import android.text.TextUtils;
 import com.shanchain.data.common.base.EventBusObject;
 import com.shanchain.data.common.ui.SetWalletPasswordActivity;
 import com.shanchain.data.common.ui.widgets.CustomDialog;
+import com.shanchain.data.common.ui.widgets.StandardDialog;
 import com.shanchain.data.common.utils.SCJsonUtils;
 import com.shanchain.data.common.utils.ThreadUtils;
 import com.shanchain.data.common.utils.ToastUtils;
@@ -30,6 +31,7 @@ import okhttp3.Response;
 public abstract class SCHttpStringCallBack extends Callback<String> {
     private Context mContext;
     public CustomDialog mCustomDialog;
+    public StandardDialog mStandardDialog;
 
 
     public SCHttpStringCallBack() {
@@ -45,20 +47,11 @@ public abstract class SCHttpStringCallBack extends Callback<String> {
     public String parseNetworkResponse(Response response, int id) throws Exception {
         String result = response.body().string();
         final String code = SCJsonUtils.parseCode(result);
+//        final String code = NetErrCode.WALLET_NOT_CREATE;
         final String msg = SCJsonUtils.parseMsg(result);
-//        if (mContext != null) {
-//            String data = SCJsonUtils.parseData(result);
-//            if (!TextUtils.isEmpty(data)) {
-//                String characterInfo = SCJsonUtils.parseString(data, "characterInfo");
-//                if (!TextUtils.isEmpty(characterInfo)) {
-//                    boolean isBindPwd = SCJsonUtils.parseBoolean(characterInfo, "isBindPwd");
-//                    boolean allowNotify = SCJsonUtils.parseBoolean(characterInfo, "allowNotify");
-//                    if (isBindPwd == false) {
-//                        code = NetErrCode.WALLET_PASSWORD_INVALID;
-//                    }
-//                }
-//            }
-//        }
+        if (mContext != null) {
+
+        }
         switch (code) {
             case NetErrCode.COMMON_SUC_CODE:
                 if (mContext != null) {
@@ -138,23 +131,40 @@ public abstract class SCHttpStringCallBack extends Callback<String> {
                     mContext.startActivity(setWalletPassword);
                     Activity activity = (Activity) mContext;
                 }
-
                 break;
             case NetErrCode.WALLET_PASSWORD_INVALID:
                 if (mContext != null) {
                     //上传密码图片弹窗
-//                    final CustomDialog showPasswordDialog = new com.shanchain.data.common.ui.widgets.CustomDialog(mContext, true, 1.0,
-//                            R.layout.dialog_bottom_wallet_password,
-//                            new int[]{R.id.iv_dialog_add_picture, R.id.tv_dialog_sure});
                     EventBusObject busObject = new EventBusObject(NetErrCode.WALLET_PHOTO, mCustomDialog);
                     EventBus.getDefault().post(busObject);
                 }
                 break;
             case NetErrCode.WALLET_NOT_CREATE:
                 if (mContext != null) {
-                    Intent ScWebView = new Intent(Intent.ACTION_VIEW, Uri.parse("activity://qianqianshijie:80/webview"));
-                    mContext.startActivity(ScWebView);
-                    Activity activity = (Activity) mContext;
+                    ThreadUtils.runOnMainThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            mStandardDialog = new StandardDialog(mContext);
+                            mStandardDialog.setStandardTitle("  ");
+                            mStandardDialog.setStandardMsg("您尚未开通马甲钱包，开通后方可使用该功能");
+                            mStandardDialog.setCancelText("返回");
+                            mStandardDialog.setSureText("去开通");
+                            mStandardDialog.setCallback(new com.shanchain.data.common.base.Callback() {
+                                @Override
+                                public void invoke() {
+                                    Intent ScWebView = new Intent(Intent.ACTION_VIEW, Uri.parse("activity://qianqianshijie:80/webview"));
+                                    mContext.startActivity(ScWebView);
+                                    Activity activity = (Activity) mContext;
+                                }
+                            }, new com.shanchain.data.common.base.Callback() {
+                                @Override
+                                public void invoke() {
+                                    mStandardDialog.dismiss();
+                                }
+                            });
+                            mStandardDialog.show();
+                        }
+                    });
                 }
                 break;
             default:
